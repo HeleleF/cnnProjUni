6461b90fa7512aca2d1c740c723c07f5788b56dc
Chris Rebbelin <s0548921@htw-berlin.de>
2018-07-13 23:09:19 +0200

    rinne

--- code/my_cnn_test.py
+++ code/my_cnn_test.py
@@ -1,4 +1,4 @@
-#pylint: disable=C0111
+#pylint: disable=C0111,E1101
 import os
 
 import cv2
@@ -6,36 +6,66 @@ import numpy as np
 from keras.models import load_model
 from keras.preprocessing.image import img_to_array
 
+import tensorflow as tf
+
+if tf.test.is_built_with_cuda():
+
+    from keras.backend.tensorflow_backend import set_session # pylint: disable=C0412
+
+    CFG = tf.ConfigProto()
+    CFG.gpu_options.per_process_gpu_memory_fraction = 0.4 #0.4 seems to work
+    set_session(tf.Session(config=CFG))
+
 MODEL = load_model("mymodel.dat")
 CLASSES = os.listdir("./flowers")
 TEST_DIR = "./flowers_test"
-IMG_SIZE = 64
+IMG_SIZE = 32
 
+LINE_HEIGHT = 30
+
+X_OFFSET = 10
+Y_OFFSET = 30
+
+WRITE_RESULT = False
+
+# loop over all files in the test directory...
 for ifile in os.listdir(TEST_DIR):
-    if ifile.endswith(".jpg"):
 
-        print(ifile)
+    # ...only take the images
+    if ifile.endswith(".jpg"):
 
         # load the image
         img = cv2.imread(os.path.join(TEST_DIR, ifile))
         out = img.copy()
 
         # prepare image
-        img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+        img = cv2.resize(img, (IMG_SIZE, IMG_SIZE), interpolation=cv2.INTER_AREA)
         img = img.astype("float") / 255.0
         img = img_to_array(img)
         img = np.expand_dims(img, axis=0)
 
-        # predict
-        proba = MODEL.predict(img)[0]
-        idx = np.argmax(proba)
+        # classify it using the model
+        probabilities = MODEL.predict(img)[0]
+
+        # sort results
+        sorted_idx = np.argsort(probabilities)[::-1]
+        results = ["{0:s} : {1:3.2f}".format(CLASSES[ix], probabilities[ix]) for ix in sorted_idx if probabilities[ix] > 0.005]
+
+        print("Prediction for {0:s}: ".format(ifile))
+
+        # print results on the image
+        for i, line in enumerate(results):
+            cv2.putText(out, line, (X_OFFSET, Y_OFFSET + i * LINE_HEIGHT),
+                        fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1,
+                        thickness=1, color=(0, 0, 255))
+
+            print("\t" + line)
 
         # show
-        text = "{:s} : {:.2f}".format(CLASSES[idx], proba[idx])
-        cv2.putText(out, text, (10, 30), fontFace=cv2.FONT_HERSHEY_DUPLEX,
-                    fontScale=1, thickness=2, color=(255, 0, 0))
-
         cv2.imshow(ifile, out)
+
+        if WRITE_RESULT:
+            cv2.imwrite("./images/classified" + ifile, out)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- code/my_cnn_train.py
+++ code/my_cnn_train.py
@@ -6,17 +6,18 @@ import cv2
 import numpy as np
 import tensorflow as tf
 
+from itertools import cycle
+
 import matplotlib.pyplot as plt
 
 from keras.backend.tensorflow_backend import set_session
 from keras.callbacks import Callback
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.layers.core import Dense, Dropout, Flatten
-from keras.models import Sequential
+from keras.models import Sequential, Model
 from keras.preprocessing.image import ImageDataGenerator, img_to_array
-from keras.optimizers import SGD
 
-from sklearn.metrics import f1_score, precision_score, recall_score, confusion_matrix
+from sklearn.metrics import f1_score, precision_score, recall_score, confusion_matrix, roc_curve, auc
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelBinarizer
 
@@ -25,8 +26,6 @@ config.gpu_options.per_process_gpu_memory_fraction = 0.6 #0.4 seems to work
 set_session(tf.Session(config=config))
 
 IMG_SIZE = 32
-
-clas_names = []
 
 # pylint: disable=
 class MyMetrics(Callback):
@@ -45,7 +44,7 @@ class MyMetrics(Callback):
         self.validation_data = (tX, tY)
 
     def on_epoch_end(self, epoch, logs=None):
-   
+
         val_predict = (np.asarray(self.model.predict(self.validation_data[0]))).round()
         val_targ = self.validation_data[1]
 
@@ -64,40 +63,52 @@ class MyMetrics(Callback):
 
 def create_model(input_shape=(32, 32, 3), n_classes=5, show=False):
     '''
-    #use convolutional layers and 1 fully-connected layer
+    create the cNN model
     '''
 
     model = Sequential()
     model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=input_shape))
-    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    #model.add(Dropout(0.2))
+
+    model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Dropout(0.2))
 
     model.add(Conv2D(16, (3, 3), padding='same', activation='relu'))
-    model.add(Conv2D(16, (3, 3), activation='relu'))
+    model.add(Conv2D(16, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(16, (3, 3), padding='same', activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    #model.add(Dropout(0.2))
 
-    model.add(Conv2D(8, (3, 3), padding='same', activation='relu'))
-    model.add(Conv2D(8, (3, 3), activation='relu'))
+    model.add(Conv2D(16, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(16, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(16, (3, 3), padding='same', activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+ 
     model.add(Dropout(0.2))
 
     model.add(Flatten())
     model.add(Dense(256, activation='relu'))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(128, activation='relu'))
     model.add(Dropout(0.2))
 
     model.add(Dense(n_classes, activation='softmax'))
 
-    if show: 
+    if show:
         model.summary()
- 
+
     return model
 
 
 def load_data(img_dir):
     '''
-    load and prepare the dataset
+    loads and prepares the dataset
     '''
     tdata = []
     tlabels = []
@@ -109,15 +120,13 @@ def load_data(img_dir):
             # get full path to image
             img_file = os.path.join(root, filename)
 
-            # read image and resize
+            # prepare image
             image = cv2.imread(img_file)
-            rimage = cv2.resize(image, (IMG_SIZE, IMG_SIZE))
-
-        	# convert image and append to data
+            rimage = cv2.resize(image, (IMG_SIZE, IMG_SIZE), interpolation=cv2.INTER_AREA)
             rimage = img_to_array(rimage)
             tdata.append(rimage)
 
-            # append corresponding label
+            # prepare corresponding label
             label = img_file.split(os.path.sep)[-2]
             tlabels.append(label)
 
@@ -136,6 +145,9 @@ def load_data(img_dir):
 
 
 def prepare_data(main_img_dir):
+    '''
+    populates data and labels 
+    '''
 
     if os.path.exists("flowers_data.npy") and os.path.exists("flowers_labels.npy"):
 
@@ -148,24 +160,17 @@ def prepare_data(main_img_dir):
         print("[INFO] Loading dataset...")
         data, labels = load_data(main_img_dir)
 
-    return train_test_split(data, labels, test_size=0.25)
+    return train_test_split(data, labels, test_size=0.2), os.listdir(main_img_dir)
 
 
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
+def plot_confusion_matrix(cm, classes, normalize=False, title='ConfMatrix', cmap=plt.cm.Blues):
     """
-    This function prints and plots the confusion matrix.
+    prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
+    taken from http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html#sphx-glr-auto-examples-model-selection-plot-confusion-matrix-py
     """
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
-
-    print(cm)
 
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
@@ -184,19 +189,91 @@ def plot_confusion_matrix(cm, classes,
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
+    plt.savefig("./images/" + title + ".pdf")
 
 
+def calc_rocs(y_test, y_pred, class_names):
+    '''
+    calculates the roc curves and auc values for all
+    taken from http://scikit-learn.org/stable/auto_examples/model_selection/plot_roc.html#sphx-glr-auto-examples-model-selection-plot-roc-py
+    '''
+    ###################################################################################
+
+    # Compute ROC curve and ROC area for each class
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+
+    n_classes = len(class_names)
+
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_pred[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # Compute micro-average ROC curve and ROC area
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_pred.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+    # First aggregate all false positive rates
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+    # Then interpolate all ROC curves at this points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += np.interp(all_fpr, fpr[i], tpr[i])
+
+    # Finally average it and compute AUC
+    mean_tpr /= n_classes
+
+    # Compute macro-average ROC curve and ROC area
+    fpr["macro"] = all_fpr
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+    # Plot all ROC curves
+    plt.figure()
+    plt.plot(fpr["micro"], tpr["micro"], label='micro-average ROC curve (area = {0:0.2f})'.format(roc_auc["micro"]), color='red', linestyle=':', linewidth=4)
+    plt.plot(fpr["macro"], tpr["macro"], label='macro-average ROC curve (area = {0:0.2f})'.format(roc_auc["macro"]), color='blue', linestyle=':', linewidth=4)
+
+    plt.plot([0, 1], [0, 1], 'k--', lw=2)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC-AUC (micro and macro average)')
+    plt.legend(loc="lower right")
+    plt.savefig("./images/ROC-AUC(micro and macro average).pdf")
+    #plt.show()
+
+    colors = ['aqua', 'darkorange', 'green', 'red', 'yellow']
+
+    plt.figure()
+    for i, color in zip(range(n_classes), colors):
+        plt.plot(fpr[i], tpr[i], color=color, lw=2, label='ROC curve for {0:s} (area = {1:0.2f})'.format(class_names[i], roc_auc[i]))
+
+    plt.plot([0, 1], [0, 1], 'k--', lw=2)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC-AUC (all classes)')
+    plt.legend(loc="lower right")
+    plt.savefig("./images/ROC-AUC(all classes).pdf")
+    plt.show()
+
+    ###################################################################################
+
+    
 def show_stats(his, metr):
 
-    fig = plt.figure()
     plt.plot(his.history['acc'])
     plt.plot(his.history['val_acc'])
     plt.title('model accuracy')
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
-    plt.savefig("accuracy.png")
-    plt.show()    
+    plt.savefig("./images/accuracy.pdf")
+    plt.show()
 
     plt.plot(his.history['loss'])
     plt.plot(his.history['val_loss'])
@@ -204,8 +281,8 @@ def show_stats(his, metr):
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
-    plt.savefig("loss.png")
-    plt.show() 
+    plt.savefig("./images/loss.pdf")
+    plt.show()
 
     plt.plot(metr.val_precisions)
     plt.plot(metr.val_recalls)
@@ -213,31 +290,33 @@ def show_stats(his, metr):
     plt.ylabel('precision/recall')
     plt.xlabel('epoch')
     plt.legend(['precision', 'recall'], loc='upper left')
-    plt.savefig("prec_rec.png")
+    plt.savefig("./images/prec_rec.pdf")
     plt.show()
-    
+
     plt.plot(metr.val_f1s)
     plt.title('model f1 score')
     plt.ylabel('f1 score')
     plt.xlabel('epoch')
     plt.legend(['f1 score'], loc='upper left')
-    plt.savefig("f1score.png")
+    plt.savefig("./images/f1score.pdf")
     plt.show()
-    
+
 
 def start_main():
 
     # get data for train and test
-    (trainX, testX, trainY, testY) = prepare_data("./flowers")
+    (x_train, x_test, y_train, y_test), class_names = prepare_data("./flowerOrig")
 
-    print("[INFO] Using {:d} samples for training and {:d} samples for testing".format(trainX.shape[0], testX.shape[0]))
+    print("[INFO] Using {:d} samples for training and {:d} samples for testing".format(x_train.shape[0], x_test.shape[0]))
 
-    model1 = create_model(input_shape=trainX.shape[1:], n_classes=5, show=True)
+    model1 = create_model(input_shape=x_train.shape[1:], n_classes=len(class_names), show=True)
+
     batch_size = 64 # smaller -> slower
-    epochs = 100 # higher -> nearly always useless after some point around 100 or less -> stagnant curve
+    epochs = 100 # higher -> nearly always useless after some point around 100 or less
 
     print("[INFO] Compiling  model...")
-    model1.compile(optimizer="adam", loss='categorical_crossentropy', metrics=['accuracy']) # loss -> categorical because mutlilabel, optimizer -> no big changes
+    # loss -> categorical because mutlilabel, optimizer -> no big changes
+    model1.compile(optimizer="rmsprop", loss='categorical_crossentropy', metrics=['accuracy']) 
 
     # construct the image generator for data augmentation
     aug = ImageDataGenerator(
@@ -248,42 +327,45 @@ def start_main():
         horizontal_flip=True,  # randomly flip images
         vertical_flip=True)  # randomly flip images
 
-    mymetrics = MyMetrics(testX, testY)
-
+    mymetrics = MyMetrics(x_test, y_test)
 
     print("[INFO] Training the model...")
-    history = model1.fit_generator(aug.flow(trainX, trainY, batch_size=batch_size),
-                                   validation_data=(testX, testY),
+    history = model1.fit_generator(aug.flow(x_train, y_train, batch_size=batch_size),
+                                   validation_data=(x_test, y_test),
 	                                  epochs=epochs, verbose=1, callbacks=[mymetrics])
 
     print("[INFO] Evaluating the model...")
-    scores = model1.evaluate(testX, testY, verbose=1)
-
-    y_pred = model1.predict(testX)
-
-    cnf_matrix = confusion_matrix(testY, y_pred)
-
-    # Plot non-normalized confusion matrix
-    plt.figure()
-    plot_confusion_matrix(cnf_matrix, classes=class_names,
-                      title='Confusion matrix, without normalization')
-
-    # Plot normalized confusion matrix
-    plt.figure()
-    plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
-                      title='Normalized confusion matrix')
-
-    plt.show()
+    scores = model1.evaluate(x_test, y_test, verbose=1)
 
     print("Accuracy: {:.4f}".format(scores[1]))
     print("Precision: {:.4f}".format(mymetrics.val_precisions[-1]))
     print("Recall: {:.4f}".format(mymetrics.val_recalls[-1]))
     print("F1 score: {:.4f}".format(mymetrics.val_f1s[-1]))
 
+    show_stats(history, mymetrics)
+
+    # confusion matrix
+    y_pred = model1.predict(x_test)
+
+    cnf_matrix = confusion_matrix(np.argmax(y_test, axis=1), np.argmax(y_pred, axis=1))
+
+    # Plot non-normalized confusion matrix
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=class_names,
+                      title='Confusion matrix without normalization')
+
+    # Plot normalized confusion matrix
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
+                      title='Normalized confusion matrix')
+
+    #plt.show()
+
+    calc_rocs(y_test, y_pred, class_names)
+
     print("[INFO] Saving the model...")
     model1.save("mymodel.dat")
 
-    show_stats(history, mymetrics)
     print("[INFO] Finished!")
 
 
